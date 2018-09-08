8415a763d39b7ec1bb970dbe7c59249ed7d07343
Slavik <shudenkoslavik@gmail.com>
2018-09-09 02:05:12 +0300

    Fixed Player/bots healthBar

--- Assets/Scripts/BotScripts/Spawnedbothp.cs
+++ Assets/Scripts/BotScripts/Spawnedbothp.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Spawnedbothp: MonoBehaviour {
-    public int hp;
+    public Image HealthBarFill;
+    public float hp;
     public static byte Countofkilled = 0;
-    private int maxbothp = 100;
+    public float maxbothp = 100f;
     public GameObject bot;
     public byte boxdropchance;
 
@@ -14,21 +15,25 @@ public class Spawnedbothp: MonoBehaviour {
         hp = maxbothp;
 		
 	}
-	void Update () {
-      if (hp <= 0)
+	void Update ()
+    {
+        Debug.Log(HealthBarFill.fillAmount);
+        HealthBarFill.fillAmount = hp / maxbothp;
+        if (hp <= 0)
         {
             Countofkilled += 1;
             Destroy(bot);
         }
+       
 
     }
     void OnTriggerEnter2D(Collider2D collider)
     {
-     if(collider.gameObject.tag == "Rocketmissle")
+        if(collider.gameObject.tag == "Rocketmissle")
         {
             hp -= 15;
         }
-     if(collider.gameObject.tag == "blaster")
+        if(collider.gameObject.tag == "blaster")
         {
             hp -= 25;
         }
