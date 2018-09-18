fb1a533561651534678291cf1c4e1fa211bb577b
Slavik <shudenkoslavik@gmail.com>
2018-09-18 03:04:50 +0300

    Create room fix

--- Assets/Scripts/Online/Createroom.cs
+++ Assets/Scripts/Online/Createroom.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Createroom : Photon.MonoBehaviour {
+public class Createroom : Photon.MonoBehaviour
+{
     public Text RoomName;
+    public Text textMax;
+    [SerializeField]
+    private Slider maxPlayers;
+    private byte maxplayers;
+    void Update()
+    {
+        if (PhotonNetwork.inRoom)
+        {
+            Initiate.Fade("deathmatch", Color.black, 2.0f);
+        }
+
+        maxplayers = (byte)maxPlayers.value;
+        textMax.text = ("Max players : " + maxplayers);
+        
+    }
 
     public void CreateRoom()
     {
-        RoomOptions roomOptions = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 4 };
-        PhotonNetwork.CreateRoom(RoomName.text, roomOptions, TypedLobby.Default);
+        RoomOptions roomOptions = new RoomOptions()
+        {
+            IsVisible = true,
+            IsOpen = true,
+            MaxPlayers = maxplayers,
+
+        };
+        PhotonNetwork.JoinOrCreateRoom(RoomName.text, roomOptions, TypedLobby.Default);
+        Debug.Log(roomOptions.MaxPlayers);
+        
+
+
     }
-
-
 }
