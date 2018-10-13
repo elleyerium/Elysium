bf2f799c3282a10a4a016843d95d6eef86b13731
Slavik <shudenkoslavik@gmail.com>
2018-10-13 15:22:26 +0300

    Some magic (FPS)

--- Assets/Scripts/Buttons/FPSTarget.cs
+++ Assets/Scripts/Buttons/FPSTarget.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class FPSTarget: MonoBehaviour
 {
-
+    [SerializeField]
+    private Text FPS;
     public int target = 60;
+    public float deltaTime;
 
     void Awake()
     {
@@ -14,6 +17,9 @@ public class FPSTarget: MonoBehaviour
 
     void Update()
     {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+        FPS.text = ( Mathf.Ceil(fps).ToString()+ " FPS ");
         if (Application.targetFrameRate != target)
             Application.targetFrameRate = target;
     }
