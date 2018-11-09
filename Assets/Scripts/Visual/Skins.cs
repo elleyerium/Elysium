e8bfad1f3bbf22e8682c5a15fa21d3736489b31e
Slavik <shudenkoslavik@gmail.com>
2018-11-09 15:24:19 +0300

    Simple skins script
    Script for adding and changing skins for Elysium

---
+++ Assets/Scripts/Visual/Skins.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Skins : MonoBehaviour
+{
+	[SerializeField] private Image _materialship;
+	[SerializeField] private Image _default;
+	[SerializeField] private Image _bloodydarkness;
+	[SerializeField] private Text SkinName;
+
+	public void SetUpSkin()
+	{
+		PlayerPrefs.SetString("CurrentlySkin", SkinName.text);
+		Debug.Log(PlayerPrefs.GetString("CurrentlySkin"));
+	}
+}
