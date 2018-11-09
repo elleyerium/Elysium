06f1cbf4142ad31fe791f555cb2190180f3d6120
Slavik <shudenkoslavik@gmail.com>
2018-11-09 15:23:36 +0300

    Awake skins checker before game loaded
    This script checking last skin name in db, and choose what it need to do

---
+++ Assets/Scripts/PlayerScripts/SkinsChecker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinsChecker : MonoBehaviour
+{
+
+    private SpriteRenderer spriteRenderer ;
+    [SerializeField] private Sprite _default;
+	[SerializeField] private Sprite _materialship;
+	[SerializeField] private Sprite _bloodydarkness;
+	void Awake()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		if (PlayerPrefs.GetString("CurrentlySkin") == "default")
+			spriteRenderer.sprite = _default;
+		if (PlayerPrefs.GetString("CurrentlySkin") == "materialship")
+			spriteRenderer.sprite = _materialship;
+		if (PlayerPrefs.GetString("CurrentlySkin") == "darkness")
+			spriteRenderer.sprite = _bloodydarkness;
+		Debug.Log(PlayerPrefs.GetString("CurrentlySkin"));
+		
+
+	}
+}
