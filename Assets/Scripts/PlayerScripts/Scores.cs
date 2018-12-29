bd65aa8dccc7b5a4288f7d609caacf7713d90fe3
Elleyer <shudenkoslavik@gmail.com>
2018-12-29 14:25:41 +0200

    Scores values updated

--- Assets/Scripts/PlayerScripts/Scores.cs
+++ Assets/Scripts/PlayerScripts/Scores.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Scores : MonoBehaviour {
+public class Scores : MonoBehaviour
+{
+    public static string[] data;
     public static float ScoreMultipliyer;
     public static float Currently_score;
     public static float Scoreup = 0f;
@@ -36,6 +39,16 @@ public class Scores : MonoBehaviour {
     public void ResetScore()
     {
         Scoreup = 0;
+    }
+
+    public static void SerializeScore()
+    {
+        data = new string[3];
+        data[0] = PlayerPrefs.GetString("username");
+        data[1] = Currently_score.ToString();
+        data[2] = DateTime.Now.ToString();
+        Debug.Log(data[0] + " " + data[1] + " " + data[2]);
+
     }
 
 }
