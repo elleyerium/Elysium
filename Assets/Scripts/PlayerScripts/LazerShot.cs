df4262fa83d932e0b29a4a74726cf09240fcf0ad
Elleyer <shudenkoslavik@gmail.com>
2018-12-29 14:33:22 +0200

    1-wawe laser with poiner Event

--- Assets/Scripts/PlayerScripts/LazerShot.cs
+++ Assets/Scripts/PlayerScripts/LazerShot.cs
@@ -1,33 +1,58 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Security.Policy;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class LazerShot : MonoBehaviour
 {
-    public static float lazernextFire;
+    public static float lazernextFire, delay;
+    public static int Timer;
     [SerializeField] public Transform lazer1;
     public GameObject lazer, Zero_ammo;
     private float _lazerfirstammo;
-    private Image Fill;
-    [SerializeField] private Button _hold;
+    [SerializeField]
+    public SpriteRenderer ION_Cannon;
+    [SerializeField] public Transform transform;
+    private Sprite _cannonSprite;
     private bool pointerDown;
-
+    private Vector3 temp;
+    private string ammo;
+    [SerializeField] public AudioSource _publicSource, _loopSource;
+    public AudioClip _startAudio, _shotAudio, _endAudio;
+    [SerializeField] private GameObject particles,cannon;
     void Start()
     {
+        _loopSource.loop = true;
+          _loopSource.clip = _shotAudio;
+             temp.x = 0;
+             temp.y = 0.884f;
+          ION_Cannon.size = temp;
+        _cannonSprite= ION_Cannon.GetComponent<Sprite>();
         _lazerfirstammo = Blastercount.Ammodownlazer;
     }
 
 void Update()
-    {
-        if (pointerDown)
+{
+    delay  += 1*Time.deltaTime;
+        if (pointerDown && delay > 0.85f)
         {
-            if (Time.time > lazernextFire && Blastercount.Ammodownlazer > 0)
+            cannon.SetActive(true);
+            particles.SetActive(true);
+            
+            if (temp.x <=3.28f)  // Animation function for laser.
             {
-                lazernextFire = Time.time + 0.3f;
+               temp.x = (ION_Cannon.size.x) + (18f * Time.deltaTime);
+               ION_Cannon.size = temp;
+            }
+            Blastercount.Ammodownlazer -= 10 * Time.deltaTime;
+            /*if (Time.time > lazernextFire && Blastercount.Ammodownlazer > 0)
+            {
+                lazernextFire = Time.time + 0.5f;
                 GameObject clonelazer = Instantiate(lazer, lazer1.position, lazer1.rotation);
                 clonelazer.SetActive(true);
                 Destroy(clonelazer, 4f);
@@ -38,22 +63,49 @@ void Update()
                 {
                     Zero_ammo.SetActive(true);
                 }
-            }
+            }*/
         }
         else if (Blastercount.Ammodownlazer < _lazerfirstammo && pointerDown == false )
         {
-            var ammo = Blastercount.Ammodownlazer += 1 * Time.deltaTime;
-            Blastercount.Ammodownlazer = Mathf.RoundToInt(ammo);
+            
+            if (BotDifficult.noob)
+                ammo = (Blastercount.Ammodownlazer += 2 * Time.deltaTime).ToString("F2");
+            if (BotDifficult.abitharder)
+                ammo = (Blastercount.Ammodownlazer += 3 * Time.deltaTime).ToString("F2");
+            if (BotDifficult.impossible)
+                ammo = (Blastercount.Ammodownlazer += 4 * Time.deltaTime).ToString("F2");
+
+            Blastercount.Ammodownlazer = float.Parse(ammo);
         }
+
+        if (!pointerDown || Blastercount.Ammodownlazer <=0)
+          {
+            _loopSource.Stop();
+            pointerDown = false;
+            temp.x = 0;
+            ION_Cannon.size = temp;
+                particles.SetActive(false);
+                cannon.SetActive(false);
+          }
     }
 
     public void IsPressed()
     {
+        _loopSource.Play();
+        
+        for (int i = 0; i <=1 ; i++)
+        {
+            delay = 0;
+            _publicSource.PlayOneShot(_startAudio);
+        }
         pointerDown = true;
+            
     }
 
     public void IsUnPressed()
     {
+        _publicSource.PlayOneShot(_endAudio);
+        _loopSource.Stop();
         pointerDown = false;
     }
 
