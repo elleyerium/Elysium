1c5c603e2fa6b1cf41150a7888555f021ced1342
Elleyer <shudenkoslavik@gmail.com>
2019-01-03 02:38:00 +0200

    Sensitivity rate added.

--- Assets/Scripts/PlayerScripts/Player2DControl.cs
+++ Assets/Scripts/PlayerScripts/Player2DControl.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using BeardedManStudios;
+using GooglePlayGames.Native.Cwrapper;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 using UnityEngine.SceneManagement;
@@ -7,9 +9,10 @@ using UnityEngine.UI;
 
 public class Player2DControl : MonoBehaviour
 {
-    public float MoveForce = 5;
+    public float MoveForce = 5, _firstammo, reloadRate, sensitivityRate;
     [SerializeField]
     public GameObject shot, Zero_ammo;
+    private string ammo;
     public Transform rPos01;
     public GameObject shot1;
     public Transform rPos02;
@@ -19,36 +22,75 @@ public class Player2DControl : MonoBehaviour
     [SerializeField]
     private Image Fill;
     private Rigidbody2D myBody;
-    bool IsPressed = true;
+    private bool IsPressed, IsReloading;
+    
     void Start()
     {
+        if (!PlayerPrefs.HasKey("Sensitivity"))
+            sensitivityRate = Settings.SensitivityValue;
+        else 
+            sensitivityRate = PlayerPrefs.GetFloat("Sensitivity");
+        
+        _firstammo = AmmoCounter.AmmodownRocket;
         myBody = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (IsPressed)
+            shoot();
+        else if (!IsPressed &&  AmmoCounter.AmmodownRocket < _firstammo)
+        {
+            ammo = (AmmoCounter.AmmodownRocket += 0.1f * Time.deltaTime).ToString("F3");
+            AmmoCounter.AmmodownRocket = float.Parse(ammo);
+        }
+
+        if (IsReloading)
+            Fill.fillAmount += ((Time.deltaTime /  reloadRate)) ;
     }
 
     void FixedUpdate()
     {
-        Vector3 moveVec = new Vector3((CrossPlatformInputManager.GetAxis("Horizontal") * MoveForce * 10),
+        
+
+        Vector3 moveVec = new Vector3((CrossPlatformInputManager.GetAxis("Horizontal") * MoveForce * 10), //TODO: Sensitivity Changer
                   (CrossPlatformInputManager.GetAxis("Vertical"))
                   * MoveForce * 10);
-        Vector3 lookVec = new Vector3((CrossPlatformInputManager.GetAxis("Horizontal")),
-            (CrossPlatformInputManager.GetAxis("Vertical")), 4096);
-        float turn = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector3 lookVec = new Vector3(
+            (CrossPlatformInputManager.GetAxis("Horizontal"))
+            ,
+            (CrossPlatformInputManager.GetAxis("Vertical"))
+            , 4096);
         if (lookVec.x != 0 && lookVec.y != 0)
-            myBody.transform.rotation = Quaternion.LookRotation(lookVec, Vector3.back);
-        var torque = 1;
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(lookVec, Vector3.back);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, sensitivityRate /2);
+        }
+
         myBody.AddForce(moveVec);
-        myBody.AddForce(lookVec);
+    }
+
+    public void IsClicked()
+    {
+        IsPressed = true;
+    }
+
+    public void UnClicked()
+    {
+        IsPressed = false;
     }
     public void shoot()
     {
         if (Time.time >= nextFire && AmmoCounter.AmmodownRocket > 0)
         {
+            IsReloading = true;
             if(BotDifficult.noob)
                 nextFire = Time.time + 3;
             if (BotDifficult.abitharder)
-                nextFire = Time.time + 5;
+                nextFire = Time.time + 4;
             if (BotDifficult.impossible)
-                nextFire = Time.time + 7;
+                nextFire = Time.time + 5;
+            reloadRate = nextFire - Time.time;
 
             GameObject clone = Instantiate(shot, rPos01.position, rPos01.rotation);
             GameObject clone2 = Instantiate(shot1, rPos02.position, rPos02.rotation);
@@ -57,7 +99,7 @@ public class Player2DControl : MonoBehaviour
             Destroy(clone, 5f);
             Destroy(clone2, 5f);
             AmmoCounter.AmmodownRocket -= 1;
-            Fill.fillAmount = 0.2f * Time.time;
+            Fill.fillAmount = 0;
         }
         if(AmmoCounter.AmmodownRocket <= 0)
         {
