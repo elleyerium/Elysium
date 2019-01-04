394d36cfef997e00a0413b7a6f4c42287ab3cbac
Elleyer <shudenkoslavik@gmail.com>
2019-01-04 15:46:07 +0200

    Main stylingManager

---
+++ Assets/Scripts/PlayerScripts/StylingManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StylingManager : MonoBehaviour
+{
+
+	public static bool Changewithmusic;
+	public GameObject SkinsView, BackgroundView, WeaponsView;
+	
+	void Start () 
+	{
+	  if(Changewithmusic)
+		  PlayerPrefs.SetString("NeedToChange", "true");
+	}
+
+	public void Skins()
+	{
+		BackgroundView.SetActive(false); WeaponsView.SetActive(false); SkinsView.SetActive(true);
+	}
+
+	public void Backgrounds()
+	{
+		WeaponsView.SetActive(false); SkinsView.SetActive(false); BackgroundView.SetActive(true);
+	}
+
+	public void Weapons()
+	{
+		SkinsView.SetActive(false); BackgroundView.SetActive(false); WeaponsView.SetActive(true);
+	}
+}
