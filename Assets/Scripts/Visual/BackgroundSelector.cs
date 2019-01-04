cb1be8d48d648861c6e05ce96fa240447f6eb490
Elleyer <shudenkoslavik@gmail.com>
2019-01-04 15:43:31 +0200

    Background Selector for Styling UI

---
+++ Assets/Scripts/Visual/BackgroundSelector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using  UnityEngine.UI;
+using UnityEngine;
+
+public class BackgroundSelector : MonoBehaviour
+{
+
+	[SerializeField] private Texture BackSprite;
+	public Text SelectText, Backname;
+	public void  Select() 
+	{
+		PlayerPrefs.SetString("BackgroundName", Backname.text);
+		Backgroung.r.material.SetTexture("_MainTex", BackSprite );		
+	}
+
+	void Update()
+	{
+		if (PlayerPrefs.GetString("BackgroundName") == Backname.text)
+		{
+			SelectText.text = "selected";
+			SelectText.color = Color.green;
+		}
+
+		if (PlayerPrefs.GetString("BackgroundName") != Backname.text)
+		{
+			SelectText.color = Color.white;
+			SelectText.text = "select";
+		}
+	}
+}
