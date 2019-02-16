91ce77e5d43e7a6bc62d57a3366cdbd7b7679bf9
Elleyer <shudenkoslavik@gmail.com>
2019-02-16 13:27:10 +0200

    SFX Manager for sounds created.

---
+++ Assets/Scripts/Visual/SFXManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BeardedManStudios;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+
+	public AudioSource AudioSource;
+	public AudioClip[] AudioClips;
+	public static void PlaySound(AudioClip clip)
+	{
+//
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
