739e4fcd4c5e8d03f0b5b80a6be0ae7b4129d3b6
Elleyer <shudenkoslavik@gmail.com>
2019-02-16 15:49:33 +0200

    Own update

--- Assets/Scripts/PlayerScripts/Scoreinmenu.cs
+++ Assets/Scripts/PlayerScripts/Scoreinmenu.cs
@@ -15,15 +15,15 @@ public class Scoreinmenu : MonoBehaviour
 
     void Start()
     {
-
         TextPrefab = new GameObject[10];
         for (int Prefabs = 0; Prefabs <= Count; Prefabs++)
         {
-            TextPrefab[Prefabs] = Instantiate(prefabForCreate, ScoreParent.transform.localPosition,ScoreParent.transform.rotation);
+            TextPrefab[Prefabs] = Instantiate(prefabForCreate, 
+                ScoreParent.transform.localPosition,ScoreParent.transform.rotation);
             TextPrefab[Prefabs].transform.SetParent(ScoreParent);
             TextPrefab[Prefabs].transform.localScale = new Vector3(1,1,1);
-            TextPrefab[Prefabs].transform.localPosition = new Vector3(TextPrefab[Prefabs].transform.position.x,
-                TextPrefab[Prefabs].transform.position.y, -55);
+            TextPrefab[Prefabs].transform.localPosition = new Vector3
+            (TextPrefab[Prefabs].transform.position.x,TextPrefab[Prefabs].transform.position.y, -55);
             if (PlayerPrefs.HasKey("Position" + Prefabs))
                 TextPrefab[Prefabs].GetComponent<Text>().text = PlayerPrefs.GetString("Position" + Prefabs);
         }
