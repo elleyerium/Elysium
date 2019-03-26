ebc22b575af232beb86379a7d5741014aded8168
Elleyer <shudenkoslavik@gmail.com>
2019-03-26 17:48:22 +0200

    New joystick asset by Screen (Elleyer)

---
+++ Assets/Scripts/Input/TouchCursor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Security;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchCursor : MonoBehaviour
+{
+	
+    [SerializeField]
+	private Image _touchCursor;
+	private Camera cam;
+	private Vector3 _lastposition;
+	public Vector2 startPos;
+	public Vector2 direction;
+	public bool directionChosen;
+	public Touch touch;
+
+	private void Start()
+	{
+		cam = Camera.main;
+	}
+
+	private void Update()
+	{
+		Vector3 point = new Vector3();
+		Event   currentEvent = Event.current;
+		Vector2 mousePos = new Vector2();
+		if (Input.touchCount > 0)
+		{
+			
+			touch = Input.GetTouch(0);
+			switch (touch.phase)
+			{
+				case TouchPhase.Began:
+					startPos = touch.position;
+					directionChosen = false;
+					break;
+				case TouchPhase.Moved:
+					direction = touch.position - startPos;
+					break;
+				case TouchPhase.Ended:
+					directionChosen = true;
+					break;
+
+			}
+
+		}
+		var x = touch.position.x;
+		var y = touch.position.y;
+		point = cam.ScreenToWorldPoint(new Vector3 (x,y, cam.nearClipPlane));
+		_touchCursor.transform.position = new Vector2(x,y);
+		
+	}
+
+
+}
