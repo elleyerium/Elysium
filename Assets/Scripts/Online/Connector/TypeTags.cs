ec21d7babd056c280c097dc7218ba87a468d28f1
Elleyer <shudenkoslavik@gmail.com>
2019-05-05 02:05:31 +0300

    Type of tags for some actions

--- Assets/Scripts/Online/Connector/TypeTags.cs
+++ Assets/Scripts/Online/Connector/TypeTags.cs
@@ -1,16 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using ExitGames.Client.Photon;
 using UnityEngine;
-
-public class TypeTags : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public enum TypeOfTags
+{
+	RegistrationRequest = 0,
+	LoginRequest = 1,
+	SetScoreRequest = 2,
+	GetScoreRequest = 3,
+	GetLeaderboardsRequest = 4
 }
