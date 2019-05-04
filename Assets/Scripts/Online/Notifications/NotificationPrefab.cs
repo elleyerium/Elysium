5484682e2c51e8b5d5d8bee3e8e816069c239259
Elleyer <shudenkoslavik@gmail.com>
2019-05-05 02:06:25 +0300

    Notification system for session

---
+++ Assets/Scripts/Online/Notifications/NotificationPrefab.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotificationPrefab : MonoBehaviour
+{
+	public GameObject Type, NoficationText;
+	public Sprite Warning_sprite, Info_sprite;
+	public void NoficationSettings(string NofType, string NofText)
+	{
+		switch (NofType)
+		{
+			case "Warning":
+				Type.GetComponent<Image>().sprite = Warning_sprite;
+				break;
+			case "Info":
+				Type.GetComponent<Image>().sprite = Info_sprite;
+				break; 
+		}
+		NoficationText.GetComponent<Text>().text = NofText;
+	}
+
+	public void DeleteNotification()
+	{
+		Destroy(gameObject);
+		//NotificationsCreator.StaticCountText.text = NotificationsCreator.transformInstance.childCount - 1;
+	}
+}
---
+++ Assets/Scripts/Online/Notifications/NotificationsCreator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotificationsCreator : MonoBehaviour
+{
+	public static int CountOfNotifications;
+	public Text CountText;
+	public static Text StaticCountText;
+	public Button ShowNofications, HideNotifications,DeleteNotifications;
+	public GameObject Prefab, UI;
+	public static GameObject PrefabInstance;
+	public Transform parent;
+	public static Transform transformInstance;
+
+	void Start()
+	{
+		CountOfNotifications = parent.childCount;
+		transformInstance = parent;
+		PrefabInstance = Prefab;
+		StaticCountText = CountText;
+		StartCoroutine("Updater");
+		Actions();
+	}
+
+	IEnumerator Updater()
+	{
+		while (true)
+		{
+		    CountOfNotifications = parent.childCount;
+		    CountText.text = CountOfNotifications.ToString();
+		    Debug.Log(CountOfNotifications);
+		    yield return new WaitForSeconds(2);
+	    }
+    }
+
+	void Actions()
+	{
+		ShowNofications.onClick.AddListener(() => UI.SetActive(true));
+		HideNotifications.onClick.AddListener(() => UI.SetActive(false));
+		DeleteNotifications.onClick.AddListener(() => Destroy(GameObject.FindWithTag("Notification")));
+	}
+	public static void NewNofication(string Type, string Text)
+	{
+		Debug.Log($"{Type} {Text}");
+		GameObject nofication = Instantiate(PrefabInstance);
+		nofication.transform.SetParent(transformInstance);
+		nofication.GetComponent<NotificationPrefab>().NoficationSettings(Type, Text);
+		CountOfNotifications++;
+		StaticCountText.text = transformInstance.childCount.ToString();
+	}
+}
