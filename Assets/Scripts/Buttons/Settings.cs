9b333f47791f29a2a9175d0d76f57c9846f7cde3
Elleyer <shudenkoslavik@gmail.com>
2019-05-26 14:00:49 +0300

    Ping request created!

--- Assets/Scripts/Buttons/Settings.cs
+++ Assets/Scripts/Buttons/Settings.cs
@@ -4,43 +4,47 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
 using System;
+using System.Net.NetworkInformation;
+using System.Text;
+using Ping = System.Net.NetworkInformation.Ping;
 
 [Serializable]
 public class Settings : MonoBehaviour
 {
 
-    public GameObject AllItems, MusicManagerUI, Global, Network, PlayerData,SnowSystem;
+    public GameObject AllItems, MusicManagerUI,MusicManagerPanel, Global, Network, PlayerData,SnowSystem;
     public Toggle Checkboxparticle;
-    public Text HighestScore, Times_Played,TotalScore,Ping, SensText;
+    public Text HighestScore, Times_Played,TotalScore,Ping,SensText;
     public Image CurrentlySelection;
     public static float SensitivityValue = 0.45f;
     bool ModeVisual, ModeStats, ModeNetwork;
     public Slider Sensitivity;
 
     void Start()
-    { 
+    {
              Sensitivity.minValue = 0.1f;
              Sensitivity.maxValue = 1.0f;
         if (!PlayerPrefs.HasKey("Sensitivity"))
             Sensitivity.value = 0.45f;
-        else 
+        else
             Sensitivity.value = PlayerPrefs.GetFloat("Sensitivity");
-        
+
         TotalScore.text = "Total score :  " + PlayerPrefs.GetFloat("TotalScore").ToString("F2");
         HighestScore.text = "Highest score :  " + PlayerPrefs.GetFloat("Highest score").ToString("F2");
         Times_Played.text = "Times played :  " + PlayerPrefs.GetInt("Play Counter");
     }
-    
+
     void Update()
     {
         SensitivityValue = Sensitivity.value;
         SensText.text = "Joystick sensitivity value is " + Sensitivity.value.ToString("F2") +"x";
     }
     public void Back()
-    {    
+    {
         PlayerPrefs.SetFloat("Sensitivity", SensitivityValue);
         AllItems.SetActive(true);
         MusicManagerUI.SetActive(true);
+        MusicManagerPanel.SetActive(true);
         gameObject.SetActive(false);
 
     }
@@ -73,6 +77,8 @@ public class Settings : MonoBehaviour
     }
     public void NetworkSettings()
     {
+        Ping ping = new Ping();
+        ping.Send(ConnectMasterServer.SERVER_IP);
         ModeNetwork = true;
         ModeVisual = false;
         ModeStats = false;
@@ -80,8 +86,32 @@ public class Settings : MonoBehaviour
         Global.SetActive(false);
         Network.SetActive(true);
         PlayerData.SetActive(false);
+        StartCoroutine(PingGetter(ping));
     }
+
+    IEnumerator PingGetter(Ping ping)
+    {
+        PingReply reply;
+        string data = "pingrequest";
+        byte[] buffer = Encoding.ASCII.GetBytes (data);
+
+        if (ConnectMasterServer.IsConnected)
+        {
+            reply = ping.Send(ConnectMasterServer.SERVER_IP, 250, buffer);
+            Debug.Log("Connected!");
+            if (reply.Status == IPStatus.Success)
+            {
+                Debug.Log(reply.Status + " " + reply.RoundtripTime);
+                Ping.text = $"Network latency(ping): {reply.RoundtripTime.ToString()} ms";
+            }
+            else Ping.text = "Ping more than 150 ms!";
+        }
+        yield return new WaitForSeconds(5);
+    }
+
+
     public void LogOut()
     {
+        ConnectMasterServer.Disconnect();
     }
 }
