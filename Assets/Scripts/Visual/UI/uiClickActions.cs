d2cef413dff7c5a01533747ae8ecedf18f2af4f3
Elleyer <shudenkoslavik@gmail.com>
2019-05-26 14:04:58 +0300

    action for buttons wrapping

---
+++ Assets/Scripts/Visual/UI/uiClickActions.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uiClickActions : MonoBehaviour
+{
+
+	public GameObject Panel, Exp;
+	private bool isActive;
+	public ImageWrapper Wrapper;
+
+	void Start()
+	{
+		Wrapper = Panel.GetComponent<ImageWrapper>();
+	}
+	public void OnClickProfile()
+	{
+		if (!isActive)
+		{
+			Panel.SetActive(true);
+			isActive = true;
+			Wrapper.Fade();
+		}
+		else
+		{
+			if (Wrapper.isFaded)
+			{
+				Panel.SetActive(false);
+				isActive = false;
+				Wrapper.isFaded = false;
+			}
+			else
+			{
+				Debug.Log(Wrapper.isFaded + " ");
+				Debug.Log("Should be wrapped!");
+			}
+		}
+	}
+}
