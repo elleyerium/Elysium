2e14cf391689ac7fe11c8dfceac19cb55eaf18c5
Elleyer <shudenkoslavik@gmail.com>
2019-08-01 23:11:42 +0300

    Remove all depends

--- Assets/Scripts/Game/Graphics/UI/Buttons/Hidenuttons.cs
+++ Assets/Scripts/Game/Graphics/UI/Buttons/Hidenuttons.cs
@@ -2,53 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hidenuttons : MonoBehaviour
+namespace Game.Graphics.UI.Buttons
 {
-    public GameObject start, lobby, gamemodechoose, styling, scores, text, allbuttons, paneldifficult, MusicManager, setPan,PlayerStatus, MusicManagerPanel;
+    public class Hidenuttons : MonoBehaviour
+    {
+        public GameObject start, lobby, gamemodechoose, styling, scores, text, allbuttons, paneldifficult, MusicManager, setPan,PlayerStatus, MusicManagerPanel;
+        [SerializeField] private CanvasGroup _mainMenu;
 
-    void Update()
-    {
-        Whattodoonline();
-    }
-    public void Online()
-    {
-        Whattodoonline();
-        gamemodechoose.SetActive(false);
-        lobby.SetActive(true);
-        allbuttons.SetActive(false);
+        void Update()
+        {
+            Whattodoonline();
+        }
+        public void Online()
+        {
+            Whattodoonline();
+            gamemodechoose.SetActive(false);
+            lobby.SetActive(true);
+            allbuttons.SetActive(false);
 
-    }
-    public void Offline()
-    {
-        paneldifficult.SetActive(true);
-    }
-    public void Back()
-    {
-        WhattodoBack();
-        gamemodechoose.SetActive(false);
-        PlayerStatus.SetActive(true);
+        }
+        public void Offline()
+        {
+            paneldifficult.SetActive(true);
+        }
+        public void Back()
+        {
+            WhattodoBack();
+            gamemodechoose.SetActive(false);
+            PlayerStatus.SetActive(true);
 
-    }
-    public void Onlineback()
-    {
-        allbuttons.SetActive(true);
-        Back();
-        lobby.SetActive(false);
-    }
-    private void WhattodoBack()
-    {
-        setPan.SetActive(true);
-        start.SetActive(true);
-        styling.SetActive(true);
-        scores.SetActive(true);
-        MusicManagerPanel.SetActive(true);
-        MusicManager.SetActive(true);
-    }
-    private void Whattodoonline()
-    {
-        start.SetActive(false);
-        styling.SetActive(false);
-        scores.SetActive(false);
-
+        }
+        public void Onlineback()
+        {
+            allbuttons.SetActive(true);
+            Back();
+            lobby.SetActive(false);
+        }
+        private void WhattodoBack()
+        {
+            setPan.SetActive(true);
+            start.SetActive(true);
+            styling.SetActive(true);
+            scores.SetActive(true);
+        }
+        private void Whattodoonline()
+        {
+            start.SetActive(false);
+            styling.SetActive(false);
+            scores.SetActive(false);
+        }
     }
 }
