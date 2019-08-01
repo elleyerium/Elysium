f9653b1337797d0b3ef058320c3bf10bf3355c00
Elleyer <shudenkoslavik@gmail.com>
2019-08-01 23:10:42 +0300

    Direction variables enum

---
+++ Assets/Scripts/Game/Graphics/UI/Menu/Animations/AnimDirection.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Game.Graphics.UI.Menu.Animations
+{
+        public enum Direction
+        {
+            Right,
+            Left,
+            Up,
+            Down
+        }
+}
