5b3b82c04dda3174ac1980402c06a26b2a86dd1d
Elleyer <shudenkoslavik@gmail.com>
2019-10-24 01:18:52 +0300

    Create Avatar.cs

---
+++ Assets/Scripts/Game/Online/Users/Avatar.cs
@@ -0,0 +1,18 @@
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Online.Users
+{
+    public class Avatar : MonoBehaviour
+    {
+        public Image UserAvatar;
+
+        public static Sprite GetTextureFromServer(byte[] buffer)
+        {
+            var texture = new Texture2D(54, 54);
+            texture.LoadImage(buffer, false);
+            return Sprite.Create(texture, new Rect(0,0,texture.width, texture.height), Vector2.zero);
+        }
+    }
+}
