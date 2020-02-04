832ac6e16599534b0f723d436fd68f90d504f267
Elleyer <shudenkoslavik@gmail.com>
2020-02-04 12:12:51 +0200

    Update MessageType.cs

--- Assets/Scripts/Game/Online/MessageType.cs
+++ Assets/Scripts/Game/Online/MessageType.cs
@@ -6,13 +6,23 @@ namespace Game.Online
         AuthorizationResponse = 2,
         GetConcurrentUsers = 3,
         GetConcurrentUsersResponse = 4,
-        UserConnected = 6,
-        UserDisconnected = 8,
+        UserConnected = 6, //Broadcast
+        UserDisconnected = 8, //Broadcast
         LeaderboardsRequest = 9,
         LeaderboardsResponse = 10,
-        GetPlayerStars = 11,
+        GetPlayerStats = 11,
         PlayerStatsResponse = 12,
         SendChatMessage = 13,
-        IncomingChatMessage = 14
+        IncomingChatMessage = 14, //Broadcast
+        UpdateProfileSettings = 15,
+        UpdateProfileSettingsResponse = 16,
+        GetAvatar = 17,
+        GetAvatarResponse = 18
+    }
+
+    public enum ProfileSettingsMessage
+    {
+        SetAvatar = 1,
+        ChangePassword = 2
     }
 }
