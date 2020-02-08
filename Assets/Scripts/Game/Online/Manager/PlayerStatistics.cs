6a87004048df7837bd47a97151a330302da3822d
Elleyer <shudenkoslavik@gmail.com>
2020-02-08 16:22:42 +0200

    Create PlayerStatistics.cs

---
+++ Assets/Scripts/Game/Online/Manager/PlayerStatistics.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Game.Online.Manager
+{
+    public class PlayerStatistics : MonoBehaviour
+    {
+        public int Rank;
+        public int Level;
+        public int Exp;
+        public int Score;
+        public int SpacePoints;
+    }
+}
