8ba310395d7f4e3abd9602300d76e076a178d771
Elleyer <shudenkoslavik@gmail.com>
2020-02-15 01:55:52 +0200

    Create Lobby.cs

---
+++ Assets/Scripts/Game/Online/Multiplayer/Lobby/Lobby.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using Game.Graphics.UI.Screen.Window;
+using Game.Online.API;
+using Game.Online.Manager;
+using UnityEngine;
+using Screen = Game.Graphics.UI.Screen.Screen;
+
+namespace Game.Online.Multiplayer.Lobby
+{
+    public class Lobby : MonoBehaviour
+    {
+        public List<Room> Rooms = new List<Room>();
+        public Room CurrentRoom;
+        //public Window OwnerWindow, PlayerWindow;
+        public GameProvider Provider;
+
+        public void CreateRoom(Room room)
+        {
+            //ConnectionProvider.SendMessage(MessageType.CreateRoom, );
+            Rooms.Add(room);
+        }
+
+        public void CloseRoom(Room room)
+        {
+            Rooms.Remove(room);
+            Debug.Log($"Room {room.RoomName} has been closed!");
+        }
+
+        public void LeaveRoom(Player player)
+        {
+            CurrentRoom.Leave(player);
+        }
+
+        public void JoinRoom(Room room)
+        {
+            if (room == CurrentRoom)
+            {
+                Debug.Log("Already joined");
+                return;
+            }
+
+            CurrentRoom = room;
+            Debug.Log("Joined!");
+        }
+
+
+        public void StartGame()
+        {
+            //Provider.SetGameProvider(GameType.Online);
+        }
+    }
+}
