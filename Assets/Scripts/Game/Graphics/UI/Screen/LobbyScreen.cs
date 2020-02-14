6d118c69036e42f4ac680845d010c18376797eb9
Elleyer <shudenkoslavik@gmail.com>
2020-02-15 01:56:06 +0200

    Create LobbyScreen.cs

---
+++ Assets/Scripts/Game/Graphics/UI/Screen/LobbyScreen.cs
@@ -0,0 +1,30 @@
+using Game.Graphics.UI.Buttons;
+using Game.Graphics.UI.Screen.Window;
+using Game.Online.API;
+using Game.Online.Manager;
+using Game.Online.Multiplayer.Lobby;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Graphics.UI.Screen
+{
+    public class LobbyScreen : Screen
+    {
+        [SerializeField] private WindowButtonHandler _windowButtonHandler;
+        [SerializeField] private ConnectionProvider _connectionProvider;
+        [SerializeField] private APIManager _apiManager;
+        [SerializeField] private Lobby _lobby;
+        [SerializeField] private Button _createNewRoom;
+
+        public void Start()
+        {
+            _createNewRoom.onClick.AddListener(() => _lobby.CreateRoom(new Room
+            {
+                MinPlayers = 2,
+                MaxPlayers = 14,
+                Owner = new Online.Player("elleyer", 41),
+                RoomName = "Elleyer's awesome room"
+            }));
+        }
+    }
+}
